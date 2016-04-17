df08ffa07db0311f9d4eb8b8d29eff1349e30ece
Marshall Sosland <marshall.sosland@gmail.com>
2016-04-17 08:59:15 -0500

    set up socket io client

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import {createStore} from 'redux';
-imort {Provider} from 'react-redux';
+import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 import reducer from './reducer';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
@@ -18,6 +19,9 @@ store.dispatch({
     }
   }
 });
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state => store.dispatch({type: 'SET_STATE', state}));
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer}/>
